45d708a40bebd8b78c5af165d9b1aeb50b7d11fd
Linrena <1063588992@qq.com>
2018-06-21 15:28:57 +0800

    add the helloclass.h

---
+++ helloclass.h
@@ -0,0 +1,16 @@
+#ifndef HELLOCLASS_H
+#define HELLOCLASS_H
+
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+class Helloclass
+{
+public:
+	Helloclass();
+	~Helloclass();
+	void sayhello();
+};
+
+#endif
